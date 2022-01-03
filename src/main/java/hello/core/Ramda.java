0ef592d98e9e0b692341e47661c748cf2a9a949c
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-01-04 02:56:28 +0900

    @Qualifier

--- src/main/java/hello/core/Ramda.java
+++ src/main/java/hello/core/Ramda.java
@@ -1,2 +1,47 @@
-package hello.core;public class Ramda {
+package hello.core;
+
+@FunctionalInterface
+interface RamdaRegexp {
+    int calculate(int a, int b);
+
+}
+@FunctionalInterface
+interface RamdaRegexp2 {
+    String soutv(String a);
+}
+
+
+class Test {
+    public int plus(RamdaRegexp calc, int a, int b) {
+        int num = calc.calculate(a, b);
+        return num;
+    }
+    /*
+    public String outString(RamdaRegexp2 text, String a){
+        int result = text.soutv(a);
+        return result;
+    }
+     */
+}
+
+
+public class Ramda {
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        int result = test.plus((a,b)-> {
+           return a + b;
+        }, 15, 15);
+        System.out.println("result = " + result);
+
+
+        /*
+        test.outString((a) -> {
+         return System.out.println("a = " + a);
+        }, result);
+        */
+
+
+    }
+
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,15 +4,17 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
