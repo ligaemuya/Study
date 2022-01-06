464e780fc9462395b0237df3702c5e49da3926cd
ligaemuya <96481698+ligaemuya@users.noreply.github.com>
2022-01-06 22:51:34 +0900

    콩의 생명주기 그리고 콜백

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,34 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean
+        public NetworkClient networkClient() {
+            System.out.println("호출됨? = " + true);
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+
+    }
+
+}
